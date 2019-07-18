4db6b57208458c780cd9150457b59ad72b023e71
6haselma <6haselma@informatik.uni-hamburg.de>
2019-07-18 10:32:05 +0200

    initial commit

---
+++ Assets/Scripts/boardAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boardAnimation : MonoBehaviour {
+
+	public Material image1;
+	public Material image2;
+	public Material image3;
+
+	private MeshRenderer meshRenderer;
+	private Material material;
+
+	// Use this for initialization
+	void Awake () {
+
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.material = image1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (Time.fixedTime == 7.0f)
+		{
+			Debug.Log("7 seconds passed");
+			meshRenderer.material = image2;
+		}
+
+		if (Time.fixedTime == 15.0f)
+		{
+			Debug.Log("15 seconds passed");
+			meshRenderer.material = image3;
+		}
+	}
+}
---
+++ Assets/Scripts/scrollAnimation.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollAnimation : MonoBehaviour {
+	//TODO: alle noch reinziehen
+	public Material image1;
+	public Material image2;
+	public Material image3;
+	public GameObject scrollMesh;
+
+	private SkinnedMeshRenderer meshRenderer;
+	private Animator animator;
+	private bool lastTransitionIsRollingUp;
+	private bool thisTransitionIsRolledUp;
+	private int counter;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		//set material 1 as start material
+		meshRenderer = scrollMesh.GetComponent<SkinnedMeshRenderer>();
+		meshRenderer.material = image1;
+		//set up animator and parameters 
+		animator = GetComponent<Animator>();
+		animator.enabled = true;
+		lastTransitionIsRollingUp = false;
+		thisTransitionIsRolledUp = false;
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("RollingUp"));
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("RollingUp"))
+		{
+			lastTransitionIsRollingUp = true;
+		}
+
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("RolledUp") && lastTransitionIsRollingUp)
+		{
+			thisTransitionIsRolledUp = true;
+		}
+
+		if (lastTransitionIsRollingUp && thisTransitionIsRolledUp)
+		{
+			counter += 1;
+			lastTransitionIsRollingUp = false;
+			thisTransitionIsRolledUp = false;
+
+			if (counter == 1)
+			{
+				Debug.Log("set image 2");
+				meshRenderer.material = image2;	
+			}
+			else if (counter == 2)
+			{
+				Debug.Log("Set image 3");
+				meshRenderer.material = image3;	
+			}
+			else if (counter == 3)
+			{
+				animator.enabled = false;
+			}
+		}
+
+
+
+		/* if (animator.GetCurrentAnimatorStateInfo(0).IsName("Armature_001|RolledUp"))
+		{
+
+		}*/
+
+	}
+}
